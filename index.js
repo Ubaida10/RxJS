bfa85890fad48abf71ad859f6810e3446a2d31a4
Muhammad Abu Ubaida Aljerah <123815303+Ubaida10@users.noreply.github.com>
2025-06-10 14:40:46 +0500

    creating simple observable, observer and operator

---
+++ index.js
@@ -0,0 +1,85 @@
+import { map, Observable } from "rxjs";
+
+const users = {
+    data:[
+        {
+            status: "inactive",
+            age: 14
+        },
+        {
+            status: "inactive",
+            age: 24
+        },
+        {
+            status: "active",
+            age: 54
+        },
+        {
+            status: "active",
+            age: 54
+        },
+        {
+            status: "active",
+            age: 74
+        },
+        {
+            status: "active",
+            age: 18
+        },
+        {
+            status: "active",
+            age: 51
+        },
+        {
+            status: "inactive",
+            age: 14
+        },
+        {
+            status: "active",
+            age: 58
+        },
+        {
+            status: "active",
+            age: 26
+        },
+        {
+            status: "active",
+            age: 24
+        }
+    ]
+}
+
+
+// Observable will emit a value and sent it to observer via subscribe method
+
+const observable = new Observable((subscriber)=>{
+    subscriber.next(users);
+}).pipe(
+    map((value)=>{
+        console.log("1) Value received from observable: ", value);
+        return value.data
+    }),
+    map((data)=>{
+        console.log("2) Data received from operator: ", data);
+        return data.filter((user) => user.status === "active" && user.age > 18);
+    }),
+);
+
+
+// The observer will receive the value and log it to the console
+const observer = {
+    next: (value)=>{
+        console.log("Value received", value);
+    },
+
+    error: (value)=>{
+        console.error("Error received");
+    },
+
+    complete: ()=>{
+        console.log("Completed");
+    }
+};
+
+
+observable.subscribe(observer);
